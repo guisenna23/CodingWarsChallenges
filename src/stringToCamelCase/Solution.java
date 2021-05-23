289309f6de9ea7b311d4eb7a77fe0ebca6bdadd7
Guilherme Senna <guisenna23@hotmail.com>
2021-05-23 16:07:19 -0300

    Add StringToCamelCase using StringBuilder

---
+++ src/stringToCamelCase/Solution.java
@@ -0,0 +1,26 @@
+package stringToCamelCase;
+
+import java.util.Locale;
+
+public class Solution {
+    static String toCamelCase(String s){
+        StringBuilder builder = new StringBuilder(s);
+
+        //Starts with 1 because I dont wanna convert the first letter to upperCase
+        for(int i=1;i<builder.length();i++){
+            if(builder.charAt(i)=='-' || builder.charAt(i)=='_'){
+                //Remove dashes and underscores
+                builder.deleteCharAt(i);
+
+                builder.replace(i,i+1,String.valueOf(builder.charAt(i)).toUpperCase());
+            }
+        }
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "the-stealth-warrior";
+        System.out.println(toCamelCase(s));
+
+    }
+}
